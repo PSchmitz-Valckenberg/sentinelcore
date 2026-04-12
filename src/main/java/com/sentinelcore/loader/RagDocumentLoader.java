4a03544d35628a000166cfa86935be6903bc6fc6
Benamira05 <145583236+Benamira05@users.noreply.github.com>
2026-04-12 20:44:24 +0200

    fix: seedimportexception

--- src/main/java/com/sentinelcore/loader/RagDocumentLoader.java
+++ src/main/java/com/sentinelcore/loader/RagDocumentLoader.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
+import com.sentinelcore.exception.SeedImportException;
 
 import java.io.IOException;
 import java.util.ArrayList;
